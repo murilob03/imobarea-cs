d24ab105fbe5c40ec1ac82081c622c11bdaaefaf
murilob03 <muriloviskboccardo@gmail.com>
2025-01-09 20:33:20 -0300

    modifica pagina de selecao de cadastro para fazer pequenos ajustos e adicionar funcionalidade de redirecionamento

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -1,44 +1,73 @@
-import Image from "next/image";
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
 export default function AccountType() {
+  const [accountType, setAccountType] = useState('')
+  const router = useRouter()
+
+  const handleContinue = () => {
+    if (accountType === 'cliente') {
+      router.push('/cadastro/cliente')
+    } else if (accountType === 'imobiliaria') {
+      router.push('/cadastro/imobiliaria')
+    } else if (accountType === 'agente') {
+      router.push('/cadastro/agente')
+    } else {
+      alert('Por favor, selecione um tipo de conta.')
+    }
+  }
+
   return (
     <div className="flex h-screen flex-col items-center justify-center bg-[#FAF5F0] p-6">
-      <div className="mt-[64px]">
+      <div className="mt-[64px] flex flex-col w-full max-w-sm">
         <h1 className="text-2xl font-bold mb-[48px]">SEJA BEM-VINDO!</h1>
-        <p className="text-base mb-[32px]">Crie sua conta de maneira rápida e fácil!</p>
-        <p className="mb-4 font-bold">Selecione o tipo de conta que deseja criar:</p>
-      </div>
-      <div className="flex flex-col gap-4 w-full max-w-sm">
-        <label className="flex items-center gap-3 p-4 rounded-lg ">
+        <p className="text-base mb-[32px]">
+          Crie sua conta de maneira rápida e fácil!
+        </p>
+        <p className="mb-7 font-bold">
+          Selecione o tipo de conta que deseja criar:
+        </p>
+        <label className="flex items-center gap-3 py-5 rounded-lg">
           <input
             type="radio"
             name="accountType"
+            value="cliente"
+            onChange={(e) => setAccountType(e.target.value)}
             className="appearance-none h-[30px] w-[30px] bg-[#E9D9C9] rounded-md checked:bg-[#8b5e3cd7] focus:outline-none cursor-pointer"
           />
           <span className="text-xl font-semibold">Cliente</span>
         </label>
-        <label className="flex items-center gap-3 p-4 rounded-lg ">
+        <label className="flex items-center gap-3 py-5 rounded-lg">
           <input
             type="radio"
             name="accountType"
+            value="imobiliaria"
+            onChange={(e) => setAccountType(e.target.value)}
             className="appearance-none h-[30px] w-[30px] bg-[#E9D9C9] rounded-md checked:bg-[#8b5e3cd7] focus:outline-none cursor-pointer"
           />
           <span className="text-xl font-semibold">Imobiliária</span>
         </label>
-        <label className="flex items-center gap-3 p-4 rounded-lg ">
+        <label className="flex items-center gap-3 py-5 rounded-lg">
           <input
             type="radio"
             name="accountType"
+            value="agente"
+            onChange={(e) => setAccountType(e.target.value)}
             className="appearance-none h-[30px] w-[30px] bg-[#E9D9C9] rounded-md checked:bg-[#8b5e3cd7] focus:outline-none cursor-pointer"
           />
           <span className="text-xl font-semibold">Agente Imobiliário</span>
         </label>
-      </div >
+      </div>
       <div className="mt-auto w-full flex justify-center">
-          <a className="text-base p-4 h-[71px] w-[344px] max-w-sm bg-[#8B5E3C] text-white font-bold rounded-full flex items-center justify-center" href="/">
-            Continuar
-          </a>
-        </div>
+        <button
+          className="text-base p-4 h-[71px] w-[344px] max-w-sm bg-[#8B5E3C] text-white font-bold rounded-full flex items-center justify-center"
+          onClick={handleContinue}
+        >
+          Continuar
+        </button>
+      </div>
     </div>
-  );
+  )
 }
